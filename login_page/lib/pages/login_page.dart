ee77d519a0583e353c6dc06a8dd43f3a7e95cf40
JFmaia <jfmaia741@gmail.com>
2021-09-29 13:13:37 -0300

    Layout da tela de login.

--- login_page/lib/pages/login_page.dart
+++ login_page/lib/pages/login_page.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:login_page/components/logo.dart';
 
 class LoginPage extends StatefulWidget {
@@ -9,64 +11,126 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  bool _isVisible = false;
+  final bool _isVisible = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.white,
-        width: double.infinity,
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-        child: Column(
-          children: [
-            const Padding(
-              padding: EdgeInsets.only(
-                top: 50,
-              ),
-              child: Logo(),
-            ),
-            const SizedBox(
-              height: 40,
-            ),
-            TextField(
-              obscureText: !_isVisible,
-              decoration: InputDecoration(
-                //Icone de visualizar a senha.
-                suffixIcon: IconButton(
-                  icon: _isVisible
-                      ? const Icon(
-                          Icons.visibility,
-                          color: Colors.black,
-                        )
-                      : const Icon(
-                          Icons.visibility_off,
-                          color: Colors.grey,
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const Logo(),
+              Padding(
+                padding: const EdgeInsets.all(30),
+                child: Column(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Colors.blueGrey,
+                            blurRadius: 20.0,
+                            offset: Offset(0, 10),
+                          ),
+                        ],
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Column(
+                        children: [
+                          Container(
+                            padding: const EdgeInsets.all(8.0),
+                            decoration: const BoxDecoration(
+                              border: Border(
+                                bottom: BorderSide(color: Colors.black12),
+                              ),
+                            ),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                hintText: "Email or Phone number",
+                                hintStyle: TextStyle(color: Colors.grey[400]),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            padding: const EdgeInsets.all(8.0),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                hintText: "Password",
+                                hintStyle: TextStyle(color: Colors.grey[400]),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 6,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 170),
+                      child: TextButton(
+                        child: const Text(
+                          "Forgot Password?",
+                          style: TextStyle(
+                            color: Colors.blueGrey,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                  //Mudando o estado do icone de visualização da senha.
-                  onPressed: () {
-                    setState(() {
-                      _isVisible = !_isVisible;
-                    });
-                  },
+                        onPressed: () {},
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                        height: 50,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          gradient: const LinearGradient(
+                            colors: [
+                              Colors.black12,
+                              Colors.blueGrey,
+                            ],
+                          ),
+                        ),
+                        child: const Center(
+                          child: Text(
+                            "Login",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    Center(
+                      child: TextButton(
+                        child: const Text(
+                          "Register here!",
+                          style: TextStyle(
+                            color: Colors.blueGrey,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        onPressed: () {},
+                      ),
+                    ),
+                  ],
                 ),
-                //Forma da caixa de texto e cor.
-                border: OutlineInputBorder(
-                  borderSide: const BorderSide(color: Colors.black),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                //Forma da caixa de texto e cor, depois de selecionada.
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: const BorderSide(color: Colors.black),
-                ),
-                //Texto que fica antes da digitação.
-                hintText: "Senha",
-                //Distancia dentro da caixa de entrada de texto.
-                contentPadding:
-                    const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
